15d5f9017cadae02bbe8a96621d3ccafdd4230b4
HRegniez <henriregniez124@gmail.com>
2022-08-18 14:39:38 +0200

    update

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
-const computerChoiceDisplay = document.getElementById('computer_choice')
-const playerChoiceDisplay = document.getElementById('player_choice')
-const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('button')
-let playerChoice
-let computerChoice
+const computerChoiceDisplay = document.getElementById('computer_choice');
+const playerChoiceDisplay = document.getElementById('player_choice');
+const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
+let playerChoice;
+let computerChoice;
 
 possibleChoices.forEach(possibleChoice => addEventListener('click', (e) => {
-    playerChoice = e.target.id
-    playerChoiceDisplay.innerHTML = playerChoice
-    generateComputerChoice()
-}))
+    playerChoice = e.target.id;
+    playerChoiceDisplay.innerHTML = playerChoice;
+    generateComputerChoice();
+}));
 
 function generateComputerChoice() {
-    const randomNumber = Math.floor(Math.random() *possibleChoices.length ) +1
+    const randomNumber = Math.floor(Math.random() *possibleChoices.length ) +1;
     if(randomNumber === 1) {
-        computerChoice = 'rock'
+        computerChoice = 'rock';
+    }else if(randomNumber === 2) {
+        computerChoice = 'paper';
+    }else if(randomNumber === 3) {
+        computerChoice = 'scissors';
     }
-    if(randomNumber === 2) {
-        computerChoice = 'paper'
+    computerChoiceDisplay.innerHTML = computerChoice;
+};
+
+function getResult() {
+    if(computerChoice === playerChoice) {
+        result = 'its a draw !';
     }
-    if(randomNumber === 3) {
-        computerChoice = 'scissors'
-    }
-    computerChoiceDisplay.innerHTML = computerChoice
+    
 }
